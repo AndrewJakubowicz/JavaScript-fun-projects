033c8ce510f517dff05234787fe52446655242ba
SpyR1014 <spyr1014@gmail.com>
2016-07-05 17:14:18 +1000

    Game works but has no display

--- tdd_tic-tac-toe/js/board.js
+++ tdd_tic-tac-toe/js/board.js
@@ -8,23 +8,93 @@
     // 1 = x, 2 = o, 0 = empty
     var data = new Array(9).fill(0);
 
+    var player = true;
+    var won = false;
 
+    var gameData = {
+      p1 : new Set([]),
+      p2 : new Set([])
+    };
 
+    var winConditions = {
+      '012': false,
+      '345': false,
+      '678': false,
+      '036': false,
+      '147': false,
+      '258': false,
+      '246': false,
+      '048': false
 
+    };
 
     // ------------- Public methods ---------- //
+
+    this.placeId = function(id){
+      if ((checkEmpty(id) && !won) && (player && placeFirst(id) || placeSecond(id))) {
+
+
+        cycleWinConditions();
+
+        won && reportWin();
+
+        player = !player;
+
+
+      }
+
+    };
 
     this.getData = function(){
       return data;
     };
 
-    this.placeFirst = function(id){
+    var placeFirst = function(id){
       data[id] = !data[id] ? 1 : data[id];
+      return true;
     };
 
-    this.placeSecond = function(id){
-      data[id] = !data[id] ? 2 : data[id];
+    var placeSecond = function(id){
+      data[id] = !data[id] ? 5 : data[id];
+      return true;
     };
+
+    var checkEmpty = function(id){
+      return !data[id];
+    };
+
+    var cycleWinConditions = function(){
+      for (var winString in winConditions) {
+        if (winConditions.hasOwnProperty(winString)) {
+          winConditions.winString = updateWin(winString);
+        }
+      }
+    };
+
+    var updateWin = function(currentWinString){
+      var indexToCheck = (currentWinString.split('')).map(Number);
+      var totalSum = 0;
+      indexToCheck.forEach(function(currentIndex){
+        totalSum += data[currentIndex];
+      });
+      won = won || totalSum == 3 || totalSum == 15;
+      return totalSum;
+    };
+
+    var reportWin = function(){
+      // Call out winning player!
+      var playerText;
+      player && (function () {
+        playerText = 'X';
+        return true;
+      })() || (function () {
+        playerText = 'O'
+      })();
+
+      var text = "Player " + playerText + " WINS!";
+
+      alert(text);
+    }
 
   }
   exports.Board = Board;
--- tdd_tic-tac-toe/tests/board_test.js
+++ tdd_tic-tac-toe/tests/board_test.js
@@ -32,24 +32,52 @@ describe("Board", function(){
     });
     it("Can't access private variables", function(){
       // Need to let expect find the error.
-      expect(function(){data}).to.throw(ReferenceError, /data is not defined/);
+      expect(function(){ data }).to.throw(ReferenceError, /data is not defined/);
     })
   });
 
   describe("Game Playing", function(){
     it('Can place x on a square', function(){
       var board = new Board();
-      board.placeFirst(2);
+      board.placeId(2);
       expect(board.getData()[2]).to.equal(1);
     });
     it("Can place o on a square", function(){
       var board = new Board();
-      board.placeSecond(2);
-      expect(board.getData()[2]).to.equal(2);
-    })
+      board.placeId(1);
+      board.placeId(2);
+      expect(board.getData()[2]).to.equal(5);
+    });
+
+    it("Can't place an o on an x!", function(){
+      var board = new Board();
+      board.placeId(2);
+      board.placeId(2);
+      expect(board.getData()[2]).to.equal(1);
+    });
+
+    it("Player is swapped automatically", function(){
+      var board = new Board();
+      board.placeId(2);
+      expect(board.getData()[2]).to.equal(1);
+      board.placeId(2);
+      expect(board.getData()[2]).to.equal(1);
+      board.placeId(1);
+      expect(board.getData()[1]).to.equal(5);
+    });
   });
 
   describe("Game can be won!", function(){
+    it("Can't place move after game has won.", function(){
+      var board = new Board();
+      board.placeId(0);
+      board.placeId(4);
+      board.placeId(1);
+      board.placeId(8);
+      board.placeId(2);
+      board.placeId(5);
+      expect(board.getData()[5]).to.equal(0);
+    })
 
   });
 });
